2a635239789ff9266895b2161e86a49bce9d8f27
TONYGUDIMA <gudima.anton@yahoo.com>
2023-05-08 22:39:41 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -45,7 +45,7 @@ class App extends Component {
   };
 
   render() {
-    const { contacts, filter } = this.state;
+    const { filter } = this.state;
     const visibleContacts = this.getVisibleContacts.apply(this);
 
     return (
