4412f667dbd199309879711a0aba28675b53251b
TONYGUDIMA <gudima.anton@yahoo.com>
2023-05-12 15:37:16 +0300

    LocalStorage Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,20 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
   addContact = ({ name, number }) => {
     const { contacts } = this.state;
     if (
@@ -48,7 +62,7 @@ class App extends Component {
 
   render() {
     const { filter } = this.state;
-    const visibleContacts = this.getVisibleContacts.apply(this);
+    const visibleContacts = this.getVisibleContacts();
 
     return (
       <div
